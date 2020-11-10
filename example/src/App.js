3703ca643832789fe33349570779c4d2fe1dfb76
Glory Ludwick <lani.ludwick>
2020-11-10 12:45:24 -0800

    Added comments

--- example/src/App.js
+++ example/src/App.js
@@ -4,6 +4,21 @@
 import React, { useRef, useEffect } from 'react'
 import './App.css';
 
+//Canvas is an HTML element used to draw graphics on a web page. 
+//The element is a container for the graphics; Javascript is used to 
+//“draw” the graphics onto the canvas.
+//With React, each component owns its node, as opposed to canvas where 
+//there is only one shared node used for drawing. 
+
+//Normally in React a ref isn't needed to make an update, but the canvas 
+//is not like other DOM elements. Most DOM elements have a property like 
+//value that allows for updating directly. The canvas works with a context 
+//that allows drawing things. For that we have to use a ref, which is a 
+//reference to the actual canvas DOM element.
+
+// Use a React reference to access the canvas DOM node, and to retrieve a 
+//2D context from it; then call the drawing methods.
+
 //Custom type to store the mouse cursor coordinates
 type Coordinates = {
   x: number;
@@ -16,25 +31,34 @@ type Coordinates = {
 //The application will render the canvas element, and will allow drawing.
 function App() {
 
-  const draw = ctx => {
-    //First drawing
-    ctx.fillStyle = '#000000';
-    ctx.beginPath();
-    ctx.arc(50,100,20,0,2*Math.PI);
-    ctx.fill();
-    //ctx.fillRect(0,0, context.canvas.width, context.canvas.height)
-  }
+  // const draw = ctx => {
+  //   //First drawing
+  //   ctx.fillStyle = '#000000';
+  //   ctx.beginPath();
+  //   ctx.arc(50,100,20,0,2*Math.PI);
+  //   ctx.fill();
+  //   //ctx.fillRect(0,0, context.canvas.width, context.canvas.height)
+  // }
 
   return (
-    <Canvas draw={draw} />
+    // <Canvas draw={draw} />
+    <div>
+      <Canvas />
+    </div>
   );
 }
 
-//Receives the draw callback and returns the canvasRef
-const useCanvas = draw => {
+//useCanvas is a custom hook which returns the canvasRef
+//Usually, big components are split into several child components in React, but it's
+//not possible with canvas as there is only one canvas node.
 
-  const canvasRef = useRef(null)
+const useCanvas = () => {
+// ignore: const useCanvas = draw => {
   
+  const canvasRef = useRef(null)
+  const [paintColor, setPaintColor] = React.useState(null);
+
+
   //useEffect handles waiting for the component to 'did mount' properly before get the real canvas.
   //Right after the canvas element is available in the dom, we want to take its context and draw.
   useEffect (() => { 
@@ -45,14 +69,16 @@ const useCanvas = draw => {
     //Get the DOM canvas element to get its context object
     //A reference to the canvas context(2d) object is assigned to the context variable that allows 
     //us to interact with properties in the HTML5 Canvas API.
+    //Cretes a drawing object for our canvas. This drawing object is what we’ll be drawing on.
     const context = canvas.getContext('2d')
 
     //Not working yet:
-    const image = new Image()
-    image.src = "./brickwall.jpg"
-    image.onload = () => {
-      context.drawImage(image, 0, 0, 500, 400)
-    }      
+    const img = new Image()
+    img.src = "./src/brickwall.jpg";
+    img.onload = () => {
+      context.drawImage(img, 0, 0, 500, 400)
+    };      
+    
 
     //Store status of mouse button. Track the mouse coordinates.
     //Horizontal and vertical offsets ensure our drawing occurs at the tip 
@@ -68,7 +94,7 @@ const useCanvas = draw => {
     //handleMouseDown captures initial coordinates of cursor for later use. 
     function handleMouseDown(evt: MouseEvent) {
       mouseDown = true;
-      draw(evt);
+      // draw(evt);
       
       // start = {
       //   x: evt.clientX = canvasOffsetLeft,
@@ -97,6 +123,11 @@ const useCanvas = draw => {
           y: end.y,
         };
 
+        // start = {
+        //   x: evt.clientX - canvasOffsetLeft,
+        //   y: evt.clientY - canvasOffsetTop,
+        // };
+
         end = {
           x: evt.clientX - canvasOffsetLeft,
           y: evt.clientY - canvasOffsetTop,
@@ -120,17 +151,18 @@ const useCanvas = draw => {
     canvasRef.current.addEventListener('mousemove', handleMouseMove);
     canvasOffsetLeft = canvasRef.current.offsetLeft;
     canvasOffsetTop = canvasRef.current.offsetTop;
-      
+
+    //<button onClick={(evt) => setPaintColor(evt.target.value) }/>
+
+
     //Using the context reference variable, we can set properties needed to render our canvas background.
-    draw(context)
-    },[draw]) //Close useEffect 
-  
+    // draw(context)
+    // },[draw]) //Close useEffect 
+    },[])
 
-
-
-
-
-  return canvasRef
+  return (
+    canvasRef 
+  )
 }
 
 
@@ -142,23 +174,31 @@ const useCanvas = draw => {
 //Define canvas component
 const Canvas = props => {
 
-  const { draw, ...rest } = props
-  const canvasRef = useCanvas(draw)
+  const { draw, ...rest } = props;
+  const canvasRef = useCanvas(draw);
+  //ignore: const imageRef = useImage(draw);
+  
 
-    return <canvas 
-          id="canvas"
-          ref={canvasRef} 
-          width={500}
-          height={400}
-          style={{
-            border: '2px solid #000',
-            marginTop: 10,
-            
-          }}
+    return ( 
+          <div>
+            <canvas 
+              id="canvas"
+              ref={canvasRef} 
+              width={500}
+              height={400}
+              style={{
+                border: '2px solid #000',
+                marginTop: 10,
+                
+              }}
 
-          {...rest}/>
-    }
+              {...rest}/>
+            <img src="/brickwall.jpg" width={500} height={400}/>
+            </div>
+          )
+        }
 
+        //ignore: <img ref={imageRef} src={brickwall} className="hidden" /> 
 
 //button style='position:absolute; top:50px; left:100px; zindex:2'
 
@@ -172,6 +212,13 @@ const Canvas = props => {
 //   setCoordinates([]);
 //  };
 
+function ClearCanvas() {
+
+  //const 
+
+}
+
+
 //Upon event click (camera icon), save canvas to image file on computer
 //ctx.save()
 
@@ -181,9 +228,13 @@ const Canvas = props => {
 //context.lineWidth = 6;
 
 
+
+
+
 //Upon event click (spray paint color icon for red, green, yellow and blue), change line color.
 //context.strokeStyle = '#ff0000';
 //context.stroke();
+
 
 //Make cursor into something else using css: 
 //<body><head><style>
